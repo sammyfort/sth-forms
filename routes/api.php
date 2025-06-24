22ac90074c6ce38ba95ed3ff2150b13447795513
Emmanuel Arhinful <kofibusy@gmail.com>
2025-06-24 16:58:22 +0000

    wip

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\GitController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::post('github-webhook', GitController::class);
+
