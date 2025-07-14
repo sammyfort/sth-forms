048b6d61cc20d7c30fa12d94d07769eeab4b92c4
Emmanuel Arhinful <kofibusy@gmail.com>
2025-07-14 08:37:27 +0000

    wip

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,7 +21,29 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        $this->configureSecureUrls();
         Model::unguard();
         Model::automaticallyEagerLoadRelationships();
+    }
+
+    protected function configureSecureUrls()
+    {
+        $enforceHttps = $this->app->environment(['production']) && !$this->app->runningUnitTests();
+        URL::forceHttps($enforceHttps);
+
+        if ($enforceHttps) {
+            $this->app['request']->server->set('HTTPS', 'on');
+        }
+
+        if ($enforceHttps) {
+            $this->app['router']->pushMiddlewareToGroup('web', function ($request, $next){
+                $response = $next($request);
+                return $response->withHeaders([
+                    'Strict-Transport-Security' => 'max-age=31536000; includeSubDomains',
+                    'Content-Security-Policy' => "upgrade-insecure-requests",
+                    'X-Content-Type-Options' => 'nosniff'
+                ]);
+            });
+        }
     }
 }
