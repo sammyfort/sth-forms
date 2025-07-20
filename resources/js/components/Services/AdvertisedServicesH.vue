11e1da320329d651817a857f24c3d0c9f6c315a3
Emmanuel Arhinful <kofibusy@gmail.com>
2025-07-20 00:30:50 +0000

    wip

--- resources/js/components/Services/AdvertisedServicesH.vue
+++ resources/js/components/Services/AdvertisedServicesH.vue
@@ -6,7 +6,7 @@ import { HTMLAttributes, onMounted, ref } from 'vue';
 import { cn } from '@/lib/utils';
 import SignboardCardV1Skeleton from '@/components/skeletons/SignboardCardV1Skeleton.vue';
 import ServiceCardV1 from '@/components/Services/ServiceCardV1.vue';
-import { getPromotedSignboards } from '@/lib/api';
+import { getPromotedServices, getPromotedSignboards } from '@/lib/api';
 
 
 const plugin = Autoplay({
@@ -23,79 +23,7 @@ const processing = ref<boolean>(false)
 
 onMounted(async ()=>{
     processing.value = true
-    services.value = (await getPromotedSignboards()).signboards
-    services.value = [
-        {
-            id: 1,
-            title: "Electrician",
-            user_name: "Emmanuel Arhinful",
-            land_landmark: "Opposite Melcom",
-            city: "Sunyani",
-            business_name: "TreySoft Systems",
-            featured: "https://picsum.photos/200/300?1",
-            is_advertised: true,
-        },
-        {
-            id: 2,
-            title: "Plumber",
-            user_name: "Ama Serwaa",
-            land_landmark: "Near Vodafone Office",
-            city: "Kumasi",
-            business_name: "Serwaa Plumbing Works",
-            featured: "https://picsum.photos/200/300?2",
-            is_advertised: true,
-        },
-        {
-            id: 3,
-            title: "Carpenter",
-            user_name: "Kwame Mensah",
-            land_landmark: "Behind Shell Fuel Station",
-            city: "Accra",
-            business_name: "Mensah Woodworks",
-            featured: "https://picsum.photos/200/300?3",
-            is_advertised: false,
-        },
-        {
-            id: 4,
-            title: "Mason",
-            user_name: "Yaw Antwi",
-            land_landmark: "Close to GCB Bank",
-            city: "Takoradi",
-            business_name: "Antwi Construction",
-            featured: "https://picsum.photos/200/300?4",
-            is_advertised: true,
-        },
-        {
-            id: 5,
-            title: "Painter",
-            user_name: "Akua Dufie",
-            land_landmark: "Opposite Central Market",
-            city: "Cape Coast",
-            business_name: "Dufie Decor",
-            featured: "https://picsum.photos/200/300?5",
-            is_advertised: true,
-        },
-        {
-            id: 6,
-            title: "Mechanic",
-            user_name: "Kojo Oppong",
-            land_landmark: "Beside Total Filling Station",
-            city: "Tamale",
-            business_name: "Oppong Auto Works",
-            featured: "https://picsum.photos/200/300?6",
-            is_advertised: false,
-        },
-        {
-            id: 7,
-            title: "Welder",
-            user_name: "Esi Nyarko",
-            land_landmark: "Near STC Yard",
-            city: "Tema",
-            business_name: "Nyarko Fabrications",
-            featured: "https://picsum.photos/200/300?7",
-            is_advertised: false,
-        },
-    ]
+    services.value = (await getPromotedServices()).services
     processing.value = false
 })
 
